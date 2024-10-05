7f25eca94291f74daf9b962470fb308db5f67a0a
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-10-05 16:46:02 +0200

    reg-log-components

---
+++ src/app/auth/register/register/register.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [RouterModule, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  private fb = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    email: ['', [ Validators.required, Validators.email ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required, Validators.minLength(6) ]],
+  })
+
+  register() {
+    console.log(this.myForm.value)
+  }
+
+
+}

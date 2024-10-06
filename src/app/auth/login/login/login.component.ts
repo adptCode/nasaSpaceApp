3da5f05301ff6be1b5ea8af94be742cf5c36b596
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-10-06 19:15:07 +0200

    change-payload-auth

--- src/app/auth/login/login/login.component.ts
+++ src/app/auth/login/login/login.component.ts
@@ -17,14 +17,14 @@ export class LoginComponent {
   private router = inject(Router);
 
   public myForm: FormGroup = this.fb.group({
-    email: ['', [ Validators.required, Validators.email ]],
+    username: ['', [ Validators.required, Validators.email ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]]
   })
 
   login() {
-    const { email, password } = this.myForm.value;
+    const { username, password } = this.myForm.value;
 
-    this.authService.login(email, password).subscribe({
+    this.authService.login(username, password).subscribe({
       next: (res) => {
         console.log('Login successful:', res);
         this.router.navigate(['/survey']);
